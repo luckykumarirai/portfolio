b896bc1b91c105ee6515fbb879fd46323d8f4ba6
Lucky kumari <luckykumari112000@gmail.com>
2021-09-23 17:30:55 +0000

    improve ui in education

--- src/components/education.js
+++ src/components/education.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-
-import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CardHeader from '@mui/material/CardHeader';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     padding:"3%"
   },
-  image1:{
-     padding:'5%'
-  },
+   image1:{
+     marginLeft:"30%",
+   },
   intro:{
     marginTop:'10%',
     padding:'3%'
@@ -23,53 +26,58 @@ export default function Education() {
   const classes = useStyles();
   return (
     <>
+     <Typography variant="h4" style={{marginLeft:'3%'}}>Education 🏫 </Typography>
       <Grid container className={classes.root} direction="row" justify="flex-start">
-                    <Grid item xs={12} sm={6} >
-                    <Typography variant="h4" >Education 🏫 </Typography>
-                        <Paper>
-                        <Typography variant="h6" style={{marginTop:'4%'}}>Indian Institute of Information Technology, Manipur</Typography>
-                        
-                        <Typography style={{color:'#90a4ae',}}>⚡ Bachelor of Technology</Typography>
+               
+                <Grid item xs={12} sm={6}>
+                    <Card sx={{ maxWidth:445 }}>
+                      <CardActionArea>
+                      <CardHeader
+                        style={{backgroundColor:"#6fdced"}}
+                        title="Indian Institute of Information Technology"
+                        subheader="Aug 2018 - July 2022"
+                      />
+                      <CardMedia>
+                        <img 
+                        style={{height:'40%',width:'40%'}}
+                        className={classes.image1} 
+                        alt="i am programmer" 
+                        src={process.env.PUBLIC_URL +"/images/iiit.jpeg" }
+                        />
 
+                      </CardMedia>
+                      <CardContent>
+                        <Typography style={{color:'#90a4ae',}}>⚡ Bachelor of Technology</Typography>
                         <Typography  style={{color:'#90a4ae'}}>⚡ Computer Science and Engineering</Typography>
                         <Typography  style={{color:'#90a4ae'}}>⚡ CGPA - 8.4</Typography>
-                        <Typography  style={{color:'#90a4ae'}}>⚡ 2018 - 2022</Typography>
-
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <img className={classes.image1} style={{height:'40%',width:'30%'}} alt="i am programmer" src={process.env.PUBLIC_URL +"/images/iiit.jpeg" }/>
-                    </Grid>
-                    <Grid item xs={12} sm={6} >
-                        <Paper >
-                        <Typography variant="h6" style={{marginTop:'4%'}}>Jawahar Navodaya Vidyalaya</Typography>
-                        
-                        <Typography style={{color:'#90a4ae',}}>⚡ Senior Secondary</Typography>
-
-                        <Typography  style={{color:'#90a4ae'}}>⚡ 2015-2017</Typography>
-                        <Typography  style={{color:'#90a4ae'}}>⚡ Marks-86%</Typography>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <img className={classes.image1} style={{height:'40%',width:'20%'}} alt="i am programmer" src={process.env.PUBLIC_URL +"/images/jnv.jpeg"} />
-                    </Grid>
-                    <Grid item xs={12} sm={6} >
-                        <Paper >
-                        <Typography variant="h6" style={{marginTop:'4%'}}>Jawahar Navodaya Vidyalaya</Typography>
-                        
-                        <Typography style={{color:'#90a4ae',}}>⚡ Senior Secondary</Typography>
-
-                        <Typography  style={{color:'#90a4ae'}}>⚡ 2015-2017</Typography>
-                        <Typography  style={{color:'#90a4ae'}}>⚡ Marks-86%</Typography>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <img className={classes.image1} style={{height:'40%',width:'20%'}} alt="i am programmer" src={process.env.PUBLIC_URL +"/images/jnv.jpeg"} />
-                    </Grid>
-                    
-                    
-                   
-                
+                      </CardContent>
+                      </CardActionArea>
+                    </Card>
+                </Grid>    
+                <Grid item xs={12} sm={6}>
+                  <Card sx={{ maxWidth: 445 }}>
+                    <CardActionArea>
+                    <CardHeader
+                      style={{backgroundColor:"#6fdced"}}
+                      title="Jawahar Navodaya Vidyalaya"
+                      subheader="May 2015 - July 2017"
+                    />
+                    <CardMedia>
+                      <img 
+                      style={{height:'5%',width:'30%',marginTop:'2%'}}
+                      className={classes.image1} 
+                      alt="i am programmer" 
+                      src={process.env.PUBLIC_URL +"/images/jnv.jpeg" }
+                      />
+                    </CardMedia>
+                    <CardContent>
+                      <Typography style={{color:'#90a4ae',}}>⚡Higher Secondary & Secondary</Typography>
+                      <Typography style={{color:'#90a4ae'}}>⚡HSS -86%</Typography>
+                      <Typography style={{color:'#90a4ae'}}>⚡SSC -9.8/10</Typography>
+                    </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Grid>         
     </Grid> 
    </>
   );
